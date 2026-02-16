03462f92c4b9e54879c295ea6d13f13dbdb320c1
talshlomo3 <talshlomo33@gmail.com>
2026-02-17 00:34:07 +0200

    creating daily json file

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 
@@ -94,7 +95,15 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
+
 if __name__ == "__main__":
-    playlist_id = get_playlist_id()
-    videos_ids = get_videos_ids(playlist_id)
-    print(extract_video_data(videos_ids))
+    playlistId = get_playlist_id()
+    video_ids = get_videos_ids(playlistId)
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
