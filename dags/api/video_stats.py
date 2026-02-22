fdfb54f94ddbccdc62ac072f1c66458d7358c404
talshlomo3 <talshlomo33@gmail.com>
2026-02-22 12:02:51 +0200

    update3

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,57 +1,87 @@
 import requests
 import json
-import os
-from dotenv import load_dotenv
 from datetime import date
 
-load_dotenv(dotenv_path="./.env")
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
-MAX_RESULTS = 50
+from airflow.decorators import task
+from airflow.models import Variable
 
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
+maxResults = 50
+
+
+@task
 def get_playlist_id():
 
     try:
+
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
         response = requests.get(url)
-        response.raise_for_status()  # Check if the request was successful
+
+        response.raise_for_status()
+
         data = response.json()
-        # print(json.dumps(data, indent=4))  # Print the response for debugging
+
+        # print(json.dumps(data,indent=4))
 
         channel_items = data["items"][0]
-        channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        print(channel_playlist_id)
+        channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"][
+            "uploads"
+        ]
 
-        return channel_playlist_id
+        return channel_playlistId
+
     except requests.exceptions.RequestException as e:
         raise e
-    
-def get_videos_ids(playlist_id):
+
+
+@task
+def get_video_ids(playlistId):
+
     video_ids = []
-    page_token = None
-    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId={playlist_id}&maxResults={MAX_RESULTS}&key={API_KEY}"
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
     try:
+
         while True:
+
             url = base_url
-            if page_token:
-                url += f"&pageToken={page_token}"
+
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
             response = requests.get(url)
-            response.raise_for_status()  # Check if the request was successful
+
+            response.raise_for_status()
+
             data = response.json()
+
             for item in data.get("items", []):
                 video_id = item["contentDetails"]["videoId"]
                 video_ids.append(video_id)
 
-            page_token = data.get("nextPageToken")
-            if not page_token:
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
                 break
 
         return video_ids
+
     except requests.exceptions.RequestException as e:
         raise e
-    
+
+
+@task
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -61,7 +91,7 @@ def extract_video_data(video_ids):
             yield video_id_lst[video_id : video_id + batch_size]
 
     try:
-        for batch in batch_list(video_ids, MAX_RESULTS):
+        for batch in batch_list(video_ids, maxResults):
             video_ids_str = ",".join(batch)
 
             url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
@@ -95,6 +125,8 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
@@ -104,6 +136,6 @@ def save_to_json(extracted_data):
 
 if __name__ == "__main__":
     playlistId = get_playlist_id()
-    video_ids = get_videos_ids(playlistId)
+    video_ids = get_video_ids(playlistId)
     video_data = extract_video_data(video_ids)
     save_to_json(video_data)
